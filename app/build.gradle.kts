aff5b9c0d29ba8af8adbab097ab6dc65aa864d5a
emrhkrmz <kirmizi.emrah.9@gmail.com>
2025-04-25 23:32:26 +0300

    hilt added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.hilt)      // ← Hilt plugin
+    alias(libs.plugins.kapt)      // ← Kapt plugin
 }
 
 android {
@@ -41,6 +44,10 @@ android {
 
 dependencies {
 
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,4 +56,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    alias(libs.plugins.hilt) apply false           // ← Hilt plugin
+    alias(libs.plugins.kapt) apply false           // ← Kapt plugin
 }
