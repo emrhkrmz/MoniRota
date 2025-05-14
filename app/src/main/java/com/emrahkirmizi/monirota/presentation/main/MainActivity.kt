52bf94ce682852986375ffc0e32e25b4214125bf
emrhkrmz <kirmizi.emrah.9@gmail.com>
2025-05-14 20:08:53 +0300

    kategori sayfası değiştirildi + eklendi

--- app/src/main/java/com/emrahkirmizi/monirota/presentation/main/MainActivity.kt
+++ app/src/main/java/com/emrahkirmizi/monirota/presentation/main/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.emrahkirmizi.monirota.R
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
-//class (Activity)
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +17,17 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // NavHostFragment'a eriş
+        // ✅ Toolbar'ı bağla
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // ✅ NavHostFragment'a eriş
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // BottomNavigationView ile eşleştir
+        // ✅ BottomNavigationView ile eşleştir
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setupWithNavController(navController)
-
-
-
-
     }
-
-
 }
