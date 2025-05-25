0424f9d26a30636a652fdef104ec463e163b2fa7
emrhkrmz <kirmizi.emrah.9@gmail.com>
2025-05-25 16:03:30 +0300

    + tamamlandı +

--- app/src/main/java/com/emrahkirmizi/monirota/presentation/main/MainActivity.kt
+++ app/src/main/java/com/emrahkirmizi/monirota/presentation/main/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.emrahkirmizi.monirota.presentation.main
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.emrahkirmizi.monirota.R
 import com.emrahkirmizi.monirota.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,31 +17,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
 
-        setSupportActionBar(binding.toolbar)
-
-        // ViewBinding ile layout'u bağla
+        // ✅ binding doğru sırayla oluşturulmalı
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Toolbar'ı setle
         setSupportActionBar(binding.toolbar)
 
-        // Status bar boşluğunu Toolbar'a uygula
-        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbar) { view, insets ->
-            val statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
-            view.setPadding(0, statusBarHeight, 0, 0)
-            insets
-        }
-
-        // Navigation bileşenlerini bağla
         val navHostFragment = supportFragmentManager
             .findFragmentById(binding.navHostFragmentContainer.id) as NavHostFragment
         val navController = navHostFragment.navController
 
         binding.bottomNavigation.setupWithNavController(navController)
     }
-
 }
